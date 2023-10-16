c5ca95145cf168ed881625a8c9b8d860b307cc24
Izabela Filipczak <izabela.filipczak@wp.pl>
2023-10-16 17:03:06 +0200

    add some screenshots

--- Selenium1.py
+++ Selenium1.py
@@ -1,22 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
 driver.get('https:/google.com')
-time.sleep(3)
+time.sleep(1)
 print('strona: ', driver.title)
 button1_accept = driver.find_element(by='id', value='L2AGLb')
 # print(button1_accept)
 # print(type)
 button1_accept.click()
-time.sleep(2)
+time.sleep(1)
 # kod z linijek od 8 do 11 można zapisać w jednej linijce:
 # driver.find_element(by='id', value='L2AGLb').click()
 search_field = driver.find_element(by='name', value='q')
-search_field.send_keys('Czy kot ma pepek?')
+search_field.send_keys('Dokąd nocą tupta jeż??')
+# search_field = driver.find_element(by='name', value="btnK")
+# search_button.submit()
+search_field.send_keys(Keys.ENTER)
+driver.get_screenshot_as_file('_zrzut_ekranu.png')
 
-search_button = driver.find_element(by='name', value="btnK")
-search_button.submit()
 
 time.sleep(1)
 driver.quit()
---
+++ Selenium2.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import datetime
+from selenium.common.exceptions import NoSuchElementException
+
+
+driver = webdriver.Chrome()
+driver.maximize_window()    # przejście na pełny ekran
+driver.get('https://www.saucedemo.com/')
+print('Nazwa strony', driver.title)
+
+def czekaj_na_id(element_id):
+    timeout = 3
+    timeout_message = f"Element o ID {element_id} nie pojawił się w czasie {timeout} s."
+    lokalizator = (By.ID, element_id)
+    znaleziono = excon.visibility_of_element_located(lokalizator)  # sprawdzamy, czy element jest
+    oczekiwator = WebDriverWait(driver, timeout)
+    return oczekiwator.until(method=znaleziono, message=timeout_message)
+    excon.visibility_of_element_located(lokalizator)
+
+def make_screenshot(okno_przegladarki):
+    today = datetime.datetime.today()
+    short_date = today.strftime('_stamp%H%M%S')
+    screen_name = 'screen' + short_date + '.png'
+    okno_przegladarki.get_screenshot_as_file(screen_name)
+
+try:
+    username_field = driver.find_element('id','user-name2')
+except:
+    print('nie znaleziono, szukam standardowych credentiali')
+    username_field = driver.find_element('id', 'user-name')
+    make_screenshot(driver)
+
+time.sleep(1)
+username_field.clear()
+username_field.send_keys('standard_user')
+
+try:
+    password_field = driver.find_element('id','user-name') # jeśli nie znajdziemy żadnego id i name, możeny poszukać po xpath:
+except NoSuchElementException:   # można wpisać, jakiś konkretny błąd, albo z dostępnych lub zaimportować dodatkowo z selenium                                                 # by: 'xpath', value: '//*[@id="password"]'
+    make_screenshot(driver)
+    print('nie znaleziono pola z hasłem')
+    raise
+
+password_field.send_keys('secret_sauce')
+
+login_button = driver.find_element('name', 'login-button')
+if not login_button.get_attribute('disabled'):  # czy przycisk aktywny
+    login_button.click()
+
+driver.get_screenshot_as_file('screen.png')
+
+make_screenshot(driver)
+
+driver.quit()
+
+'''
+z tego zrobiliśmy funkcję make_screenshot:
+today = datetime.datetime.today()
+short_date = today.strftime('_stamp%H%M%S')
+screen_name = 'screen' + short_date + '.png'
+driver.get_screenshot_as_file(screen_name)
+'''
+
+
