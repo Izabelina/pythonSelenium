3b34d0a54f79f98000f12f498cd88e64a87819be
Izabela Filipczak <izabela.filipczak@wp.pl>
2023-10-18 21:14:47 +0200

    add some OOP class

--- Selenium1.py
+++ Selenium1.py
@@ -6,13 +6,13 @@ driver = webdriver.Chrome()
 driver.get('https:/google.com')
 time.sleep(1)
 print('strona: ', driver.title)
+
 button1_accept = driver.find_element(by='id', value='L2AGLb')
 # print(button1_accept)
 # print(type)
 button1_accept.click()
 time.sleep(1)
-# kod z linijek od 8 do 11 można zapisać w jednej linijce:
-# driver.find_element(by='id', value='L2AGLb').click()
+# driver.find_element(by='id', value='L2AGLb').click() <-- kod z linijek od 8 do 11 można zapisać w jednej linijce:
 search_field = driver.find_element(by='name', value='q')
 search_field.send_keys('Dokąd nocą tupta jeż??')
 # search_field = driver.find_element(by='name', value="btnK")
